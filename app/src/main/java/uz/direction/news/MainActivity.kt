892731b9f82288162618cbd32396a66a6696b14c
Aidein <sultanovazamat985@gmail.com>
2022-06-12 16:00:33 +0500

    Retrofit implemented

--- app/src/main/java/uz/direction/news/MainActivity.kt
+++ app/src/main/java/uz/direction/news/MainActivity.kt
@@ -2,10 +2,23 @@ package uz.direction.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.direction.news.data.network.RetrofitService
+import uz.direction.news.data.repository.NewsRepository
+import uz.direction.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val repository = NewsRepository(RetrofitService.newsService)
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        repository.getNews("us")
+
+        repository.newsLiveData.observe(this){ news ->
+            binding.newsHeader.text = news.articles[0].title
+        }
     }
 }
---
+++ app/src/main/java/uz/direction/news/data/network/news/NewsService.kt
@@ -0,0 +1,13 @@
+package uz.direction.news.data.network.news
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import uz.direction.news.data.model.News
+
+interface NewsService {
+
+    @GET("v2/top-headlines")
+    suspend fun getNews(@Query("country") country: String): Response<News>
+
+}
