8c86a244e008fd37d7d5d1451d1030d1ada574b4
Aidein <sultanovazamat985@gmail.com>
2022-06-14 17:31:59 +0500

    ViewBindingPropertyDelegate added

--- app/src/main/java/uz/direction/news/MainActivity.kt
+++ app/src/main/java/uz/direction/news/MainActivity.kt
@@ -2,14 +2,14 @@ package uz.direction.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.direction.news.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding by viewBinding(ActivityMainBinding::bind, R.id.fragment_container_view)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
