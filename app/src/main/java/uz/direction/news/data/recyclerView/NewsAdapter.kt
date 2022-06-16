debd6e198e2e2bd5b910d4c15b1125c6b25945f5
fayyo <naimovfayyoz>
2022-06-16 11:24:29 +0500

    glide

--- app/src/main/java/uz/direction/news/data/recyclerView/NewsAdapter.kt
+++ app/src/main/java/uz/direction/news/data/recyclerView/NewsAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import uz.direction.news.R
 import uz.direction.news.data.model.Article
 import uz.direction.news.data.model.News
@@ -26,27 +27,22 @@ class NewsAdapter
 
     override fun getItemCount() = articles.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val itemImage: ImageView
-        private val itemTitle: TextView
-        private val itemDetail: TextView
-
-        init {
-            itemImage = itemView.findViewById(R.id.item_image)
-            itemTitle = itemView.findViewById(R.id.item_title)
-            itemDetail = itemView.findViewById(R.id.item_details)
-        }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itemImage: ImageView = itemView.findViewById(R.id.item_image)
+        private val itemTitle: TextView = itemView.findViewById(R.id.item_title)
+        private val itemDetail: TextView = itemView.findViewById(R.id.item_details)
 
         fun onBind(article: Article) {
+            //Glide.with(itemView.context).load(article.urlToImage).placeholder(R.drawable.ic_launcher_background).into(itemImage)
             itemTitle.text = article.title
             itemDetail.text = article.content
         }
     }
 
-    @SuppressLint("NotifyDataSetChanged")
+
+
     fun setData(newList: List<Article>) {
         articles = newList
-        notifyDataSetChanged()
 
     }
 
